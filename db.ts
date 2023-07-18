07759667b44cf457e14cf92ceb1474de864a504d
waptik <itswaptik@gmail.com>
2023-07-18 20:01:07 +0000

    pushing working example

---
+++ db.ts
@@ -0,0 +1,103 @@
+import { createPentagon } from "https://raw.githubusercontent.com/skoshx/pentagon/6b9f2627685b0729d5dcf7103c19f690ff4554ec/mod.ts";
+// "https://deno.land/x/pentagon@v0.1.2/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+
+export const generateUUID = () => crypto.randomUUID();
+export const kv = await Deno.openKv();
+//------------------------------------- Models -------------------------------------//
+
+const WithDefautTimestamps = z.object({
+  createdAt: z
+    .string()
+    .datetime()
+    .default(() => new Date().toISOString()),
+  updatedAt: z
+    .string()
+    .datetime()
+    .default(() => new Date().toISOString())
+    .nullable()
+    .nullable(),
+});
+
+export const User = z
+  .object({
+    id: z.number().describe("primary"),
+    username: z.string().describe("unique").optional(),
+    gender: z.string(),
+    firstName: z.string(),
+    lastName: z.string().optional(),
+    nickname: z.string().describe("unique"),
+    enableScreening: z.boolean().default(false).optional(),
+  })
+  .merge(WithDefautTimestamps);
+
+export const Meeting = z
+  .object({
+    id: z
+      .string()
+      .uuid()
+      .describe("primary")
+      .default(() => generateUUID()),
+    title: z.string(),
+    description: z.string(),
+    slug: z
+      .string()
+      .describe("unique")
+      .min(1)
+      .default(() => generateUUID()),
+    duration: z.number().min(1),
+    price: z.number().min(5),
+    requiresConfirmation: z.boolean().default(false).optional(),
+    hidden: z.boolean().default(false).optional(),
+    metadata: z
+      .object({
+        objective: z.string().optional(),
+      })
+      .nullable()
+      .default(null),
+    // relations
+    userId: z.number(),
+  })
+  .merge(WithDefautTimestamps);
+
+export const AgreedTerms = z
+  .object({
+    id: z.number().describe("primary"),
+  })
+  .merge(WithDefautTimestamps);
+
+export const BookingReference = z
+  .object({
+    id: z
+      .string()
+      .uuid()
+      .describe("primary")
+      .default(() => generateUUID()),
+    type: z.string().min(1),
+    bookingId: z.string().uuid().optional(),
+    meetingId: z.string().uuid().optional(),
+    meetingUrl: z.string().url().optional(),
+    meetingPassword: z.string().optional(),
+  })
+  .merge(WithDefautTimestamps);
+
+//------------------------------------- Models end -------------------------------------//
+
+//------------------------------------- Database -------------------------------------//
+export const pentagon = createPentagon(kv, {
+  users: {
+    schema: User,
+    relations: {
+      meetings: ["meeting", [Meeting], "id", "userId"],
+    },
+  },
+  agreedTerms: {
+    schema: AgreedTerms,
+  },
+  bookingReference: {
+    schema: BookingReference,
+  },
+  meeting: {
+    schema: Meeting,
+  },
+});
---
+++ main.ts
@@ -0,0 +1,90 @@
+import { kv, pentagon } from "./db.ts";
+
+async function getAllKvData() {
+  console.log("calling getAllKvData");
+  const it = kv.list({ prefix: [] });
+  for await (const { key, value } of it) {
+    console.log("key=value", { key, value });
+  }
+}
+
+async function doMeetings() {
+  console.log("calling doMeetings");
+  const meeting = await pentagon.meeting.create({
+    data: {
+      title: Math.random().toString(),
+      description: Math.random().toString(),
+      userId: Math.random(),
+      duration: Math.random() * 28,
+      price: Math.random() * 100,
+    },
+  });
+  console.log({ meeting });
+  const meetings = await pentagon.meeting.findMany({});
+  console.log({ meetings });
+}
+
+async function doUser() {
+  console.log("calling doUser");
+  const genders = ["male", "female", "other"];
+  const genderIndex = Math.floor(Math.random() * 3);
+
+  const user = await pentagon.users.create({
+    data: {
+      id: Math.random(),
+      username: Math.random().toString(),
+      firstName: Math.random().toString(),
+      lastName: Math.random().toString(),
+      nickname: Math.random().toString(),
+      gender: genders[genderIndex],
+    },
+  });
+
+  console.log({ user });
+
+  const users = await pentagon.users.findMany({});
+  console.log({ users });
+}
+
+async function doBookingReference() {
+  console.log("calling doBookingReference");
+  const bookingReference = await pentagon.bookingReference.create({
+    data: {
+      meetingId: crypto.randomUUID(),
+      bookingId: crypto.randomUUID(),
+      type: "meeting",
+    },
+  });
+
+  console.log({ bookingReference });
+
+  const references = await pentagon.bookingReference.findMany({});
+  console.log({ references });
+}
+
+async function doAgreedTerms() {
+  console.log("calling doAgreedTerms");
+
+  const agreedTerms = await pentagon.agreedTerms.create({
+    data: {
+      id: Math.random(),
+    },
+  });
+
+  console.log({ agreedTerms });
+
+  const agreements = await pentagon.agreedTerms.findMany({});
+  console.log({ agreements });
+}
+
+async function main() {
+  await Promise.all([
+    getAllKvData(),
+    doMeetings(),
+    doBookingReference(),
+    doAgreedTerms(),
+    doUser(),
+  ]);
+}
+
+main().catch(console.error);
